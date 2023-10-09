9557bd26fa030673f9945bb211ef507518f35c4e
seodevv <seodev17@gmail.com>
2023-10-09 13:32:15 +0900

    add main arrivials

--- src/pages/main/Header.tsx
+++ src/pages/main/Header.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Box } from '../../components/Styled';
+import styled from 'styled-components';
 
 interface Props {
   text: string;
 }
 
+const StyledHeader = styled.p`
+  font-size: 1.2rem;
+  text-shadow: 2px 3px 7px white, 3px 7px 1px #a1a1a1;
+`;
+
 const Header = ({ text }: Props): JSX.Element => {
   return (
     <>
       <Box ma="15px" wid="100%" bg="#fff" textAlign="center">
-        <h3>{text.toUpperCase()}</h3>
+        <StyledHeader>{text.toUpperCase()}</StyledHeader>
       </Box>
     </>
   );
